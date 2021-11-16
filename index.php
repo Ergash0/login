4e967f6e9fa26266043a563100da99b6584ca2a5
Ergash0 <92915363+Ergash0@users.noreply.github.com>
2021-11-16 19:37:01 +0600

    Update index.php

--- index.php
+++ index.php
@@ -147,7 +147,8 @@ span.psw {
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
         
-      <button type="submit">Login</button>
+        <button type="submit">Login</button>
+        alert("invalid password, try again later")
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
